688bf8eb3a66c46d7f0fe27a0c13f05effc73229
simone <sorzesimone1@gmail.com>
2021-05-22 22:18:19 +0200

    removing module

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+from loader import progress
 from openpyxl import Workbook, load_workbook
 from pathlib import Path
 from converter import format_bytes
@@ -8,6 +9,9 @@ from converter import format_bytes
 workbook = Workbook()
 page = workbook.active
 column_name = [["File_Name", "Path", "Size", "Label"]]
+total = 1000
+i = 0
+
 
 input_folder = input("Enter the desired path: ")
 
@@ -20,13 +24,16 @@ if question == "yes":
 for col_name in column_name:
     page.append(col_name)
 
-for root, dirs, files in os.walk(input_folder):
-    if 'dirs_to_exclude' in globals():
-        for item in dirs_to_exclude:
-            if item in dirs:
-                dirs.remove(item)
-    else:
-        pass
+
+while i < total:
+    progress(i, total, status='Doing very long job')
+    for root, dirs, files in os.walk(input_folder):
+        if 'dirs_to_exclude' in globals():
+            for item in dirs_to_exclude:
+                if item in dirs:
+                    dirs.remove(item)
+        else:
+            pass
     for file in files:
         item = os.path.join(root, file)
         item_size = os.stat(item).st_size
@@ -34,5 +41,7 @@ for root, dirs, files in os.walk(input_folder):
         format_number = format[0]
         format_label = format[1]
         page.append([file, item, format_number, format_label])
+    i += 1
+
 
 workbook.save(filename="result.xlsx")
