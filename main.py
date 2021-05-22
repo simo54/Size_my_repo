4a2f0ad14aa32ad7a7b174de57e2d7c5eff4a5a1
simone <sorzesimone1@gmail.com>
2021-05-22 22:10:26 +0200

    testing loader

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
+import time
 from openpyxl import Workbook, load_workbook
 from pathlib import Path
 from converter import format_bytes
+from loader import printProgressBar
 
 workbook = Workbook()
 page = workbook.active
-
 column_name = [["File_Name", "Path", "Size", "Label"]]
-
-for col_name in column_name:
-    page.append(col_name)
 
 input_folder = input("Enter the desired path: ")
 
@@ -18,6 +16,9 @@ question = input("Do you want to exclude any folders? yes/no: ")
 if question == "yes":
     input_dirs = input("Write the repos (space for separation): ")
     dirs_to_exclude = input_dirs.split()
+
+for col_name in column_name:
+    page.append(col_name)
 
 for root, dirs, files in os.walk(input_folder):
     if 'dirs_to_exclude' in globals():
