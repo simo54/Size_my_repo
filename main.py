bda69d65d2002a1614c7c298571238ed886d634a
simone <sorzesimone1@gmail.com>
2021-05-22 18:07:58 +0200

    providing xlsx file

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
+import os
+from openpyxl import Workbook, load_workbook
 from pathlib import Path
 from converter import format_bytes
-import os
+
+workbook = Workbook()
+page = workbook.active
 
 input_folder = input("Enter the desired path: ")
 
-dirs_to_exclude = ['.git', 'node_modules']
+question = input("Do you want to exclude any folders? yes/no: ")
+
+if question == "yes":
+    dirs_to_exclude = ['.git', 'node_modules']
+
+column_name = [["File_Name", "Path", "Size", "Label"]]
+
+for col_name in column_name:
+    page.append(col_name)
 
 for root, dirs, files in os.walk(input_folder):
     # Exclude directories
-    for item in dirs_to_exclude:
-        if item in dirs:
-            dirs.remove(item)
+    if "dirs_to_exclude" in globals():
+        for item in dirs_to_exclude:
+            if item in dirs:
+                dirs.remove(item)
     for file in files:
         item = os.path.join(root, file)
         item_size = os.stat(item).st_size
-        print(format_bytes(item_size))
-        print(item)
-        print(file)
+        format = format_bytes(item_size)
+        format_number = format[0]
+        format_label = format[1]
+        page.append([file, item, format_number, format_label])
+
+workbook.save(filename="result.xlsx")
