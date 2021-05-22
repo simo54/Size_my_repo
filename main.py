8feed7bc4a639ab895c97af9840afc19c662c07e
simone <sorzesimone1@gmail.com>
2021-05-22 22:33:41 +0200

    making some info texts

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-import time
 
-from loader import progress
 from openpyxl import Workbook, load_workbook
 from pathlib import Path
 from converter import format_bytes
@@ -9,31 +7,28 @@ from converter import format_bytes
 workbook = Workbook()
 page = workbook.active
 column_name = [["File_Name", "Path", "Size", "Label"]]
-total = 1000
-i = 0
 
+input_folder = input("\n👋 Howdy? Enter the desired path: ")
 
-input_folder = input("Enter the desired path: ")
-
-question = input("Do you want to exclude any folders? yes/no: ")
+question = input("\n❌ Do you want to exclude any folders? yes/no: ")
 
 if question == "yes":
-    input_dirs = input("Write the repos (space for separation): ")
+    input_dirs = input("\nWrite the repos (space for separation): ")
     dirs_to_exclude = input_dirs.split()
 
 for col_name in column_name:
     page.append(col_name)
 
+print("\n⏱  Loaders are expensive but trust me, I am working ^_____^")
 
-while i < total:
-    progress(i, total, status='Doing very long job')
-    for root, dirs, files in os.walk(input_folder):
-        if 'dirs_to_exclude' in globals():
-            for item in dirs_to_exclude:
-                if item in dirs:
-                    dirs.remove(item)
-        else:
-            pass
+
+for root, dirs, files in os.walk(input_folder):
+    if 'dirs_to_exclude' in globals():
+        for item in dirs_to_exclude:
+            if item in dirs:
+                dirs.remove(item)
+    else:
+        pass
     for file in files:
         item = os.path.join(root, file)
         item_size = os.stat(item).st_size
@@ -41,7 +36,7 @@ while i < total:
         format_number = format[0]
         format_label = format[1]
         page.append([file, item, format_number, format_label])
-    i += 1
 
+print("\n✔  All done, you see? ✔")
 
 workbook.save(filename="result.xlsx")
